f513e067a056d9ab12d33ddbcfba63f04ba1ea2f
avardhineni <c_ashwani.vardhineni@viasat.com>
2024-01-11 17:56:20 +0530

    script with ARA

--- Repo_creation_Scripts/test6.sh
+++ Repo_creation_Scripts/test6.sh
@@ -7,12 +7,11 @@ ARTIFACTORY_URL="https://vminds.jfrog.io/artifactory"
 USERNAME="avardhineni4@gmail.com"
 API_KEY="AKCpBrvkRp4gkkQ3BtVzZPNh8pGnZfGEhn411GUeatAKHPRExdgPZVxgU2YRTsUsCLNzRBRso"
 
-# Retrieve the latest txt file
-latest_txt_file=$(ls -t /opt/parsejsontxt/*.txt 2>/dev/null | head -1)
-echo "$latest_txt_file"
+# Set the path to the new input file
+input_file="inputs.txt"
 
 # Check if the file exists and Anonymous Read-Only Access is Yes
-if [ -f "$latest_txt_file" ] && grep -q "Anonymous Read-Only Access: Yes" "$latest_txt_file"; then
+if [ -f "$input_file" ] && grep -q "Anonymous Read-Only Access: Yes" "$input_file"; then
     # Retrieve permission target
     PERMISSION_TARGET=$(curl -u "$USERNAME:$API_KEY" -X GET "$ARTIFACTORY_URL/api/v2/security/permissions/anonymous-read-only-prod")
     echo "$PERMISSION_TARGET"
@@ -22,7 +21,7 @@ if [ -f "$latest_txt_file" ] && grep -q "Anonymous Read-Only Access: Yes" "$late
     echo "$EXISTING_REPOS"
 
     # Extract REPO_NAME from the latest txt file
-    REPO_NAME=$(cat "$latest_txt_file" | grep -m 1 "New Repository Name:" | cut -d ':' -f 2 | tr -d ' ' | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+    REPO_NAME=$(cat "$input_file" | grep -m 1 "New Repository Name:" | cut -d ':' -f 2 | tr -d ' ' | tr '[:upper:]' '[:lower:]' | tr '_' '-')
     echo "$REPO_NAME"
 
     # Append the new repository name to the existing repositories
@@ -30,10 +29,10 @@ if [ -f "$latest_txt_file" ] && grep -q "Anonymous Read-Only Access: Yes" "$late
     echo "$NEW_REPOS"
 
     # Store the updated repositories list in a file
-    echo "$NEW_REPOS" > /opt/ara/repositories_list.txt
+    echo "$NEW_REPOS" > repositories_list.txt
 
     # Print the updated repositories list
-    echo "Updated Repositories List: $NEW_REPOS_STRING"
+    echo "Updated Repositories List: $NEW_REPOS"
 
     # Update the permission target with the new repositories list
     curl -X PUT "$ARTIFACTORY_URL/api/v2/security/permissions/anonymous-read-only-prod" \
